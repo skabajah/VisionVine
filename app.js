fc1732189c747bd2346e46c5af4cffe211e4182e
Shadi Kabajah <shadi.kabajah@gmail.com>
2026-08-06 12:38:28 -0400

    pushed via [SK-MacbookAir-M1] on 8/6 12:38 PM

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 // ============================================
 
 const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxBwcgwD3p6tseMBQBFdK6ulyOyfVLD9qMD77N6oXEVRDlJLMyPuFMFcf1FYJ_69leRgw/exec";
+
 const CORS_PROXY = "https://api.allorigins.win/raw?url=";
 
 const dropZone = document.getElementById("dropZone");
